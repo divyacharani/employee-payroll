ac6cfa42d2e28fb6f39479d58e00a98b3290a4c6
unknown <divyacharaninabha@gmail.com>
2020-10-15 23:16:14 +0530

    Added: Welcome message added

---
+++ src/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayrollService {
+	
+	public static void main(String[] args) {
+		
+		//Welcome Message
+		System.out.println("Welcome to Employee Payroll Program!!");
+	}
+
+}
